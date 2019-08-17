b70230908682d81f544e5d5aeb8c08cb932cdf12
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2019-08-16 21:31:56 -0300

    Add Senha Criptografada MD5 no sistema

--- src/main/java/br/com/glp/controller/ProdutoControle.java
+++ src/main/java/br/com/glp/controller/ProdutoControle.java
@@ -1,46 +1,62 @@
 package br.com.glp.controller;
 
 import br.com.glp.dao.HibernateUtil;
+import br.com.glp.dao.MarcaDao;
+import br.com.glp.dao.MarcaDaoImpl;
 import br.com.glp.dao.ProdutoDao;
 import br.com.glp.dao.ProdutoDaoImpl;
+import br.com.glp.dao.SituacaoDao;
+import br.com.glp.dao.SituacaoDaoImpl;
 import br.com.glp.model.Marca;
 import br.com.glp.model.Produto;
 import br.com.glp.model.Situacao;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.primefaces.context.RequestContext;
 
 /**
  *
- * @author Pedrão Master
+ * @author Leo
  */
 @ManagedBean(name = "produtoC")
 @ViewScoped
 public class ProdutoControle implements Serializable {
 
-    private boolean mostrar_Toolbar;
     private Session session;
-
-    private ProdutoDao produtoDao;
+    private boolean mostrar_toolbar;
 
     private Produto produto;
     private Marca marca;
     private Situacao situacao;
 
+    private ProdutoDao produtoDao;
+
+    private DataModel<Produto> modelProdutos;
     private List<Produto> produtos;
-    private DataModel<Produto> modelProduto;
+    private List<SelectItem> marcas;
+    private List<SelectItem> situacoes;
+
+//    @PostConstruct
+//    public void inicializar() {
+//        carregaMarcas();
+//        carregaSituacoes();
+//    }
 
     public ProdutoControle() {
         produtoDao = new ProdutoDaoImpl();
     }
 
     private void abreSessao() {
-        if (session == null || !session.isOpen()) {
+        if (session == null) {
             session = HibernateUtil.abreSessao();
         } else if (!session.isOpen()) {
             session = HibernateUtil.abreSessao();
@@ -48,31 +64,30 @@ public class ProdutoControle implements Serializable {
     }
 
     public void novo() {
-        mostrar_Toolbar = !mostrar_Toolbar;
+        mostrar_toolbar = !mostrar_toolbar;
         limpar();
     }
 
     public void novaPesquisa() {
-        mostrar_Toolbar = !mostrar_Toolbar;
+        mostrar_toolbar = !mostrar_toolbar;
         limpar();
     }
 
     public void preparaAlterar() {
-        mostrar_Toolbar = !mostrar_Toolbar;
+        mostrar_toolbar = !mostrar_toolbar;
         limpar();
     }
 
     public void carregarParaAlterar() {
-        mostrar_Toolbar = !mostrar_Toolbar;
-        produto = modelProduto.getRowData();
-        marca.getProduto();
-        situacao.getProduto();
-
+        mostrar_toolbar = !mostrar_toolbar;
+        produto = modelProdutos.getRowData();
+        marca = produto.getMarca();
+        situacao = produto.getSituacao();
     }
 
     public void pesquisar() {
+        produtoDao = new ProdutoDaoImpl();
         try {
-
             abreSessao();
 
             if (!produto.getNomeProduto().equals("")) {
@@ -80,11 +95,95 @@ public class ProdutoControle implements Serializable {
             } else {
                 produtos = produtoDao.listaTodos(session);
             }
-            modelProduto = new ListDataModel(produtos);
+            modelProdutos = new ListDataModel(produtos);
+        } catch (HibernateException ex) {
+            System.err.println("Erro pesquisa Produto:\n" + ex.getMessage());
+        } finally {
+            session.close();
+        }
+    }
 
-//            produto.setNomeProduto(null);
+    private void carregaMarcas() {
+        List<Marca> todasMarcas;
+        try {
+            abreSessao();
+            marcas = new ArrayList();
+
+            MarcaDao marcaDao = new MarcaDaoImpl();
+            todasMarcas = marcaDao.listaTodos(session);
+            todasMarcas.stream().forEach((perf) -> {
+                marcas.add(new SelectItem(perf.getId(), perf.getNomeMarca()));
+            });
+        } catch (HibernateException hi) {
+            System.out.println("Erro ao carregar os marca " + hi.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public void salvarMarca() {
+        abreSessao();
+        try {
+            MarcaDao marcaDao = new MarcaDaoImpl();
+            marcaDao.salvarOuAlterar(marca, session);
+            carregaMarcas();
+        } catch (HibernateException ex) {
+            System.err.println("Erro ao Salvar nova Marca:\n" + ex.getMessage());
+        } finally {
+        }
+    }
+
+    public void salvarSituacao() {
+        abreSessao();
+        try {
+            SituacaoDao situacaoDao = new SituacaoDaoImpl();
+            situacaoDao.salvarOuAlterar(situacao, session);
+            carregaSituacoes();
+        } catch (HibernateException ex) {
+            System.err.println("Erro ao Salvar nova Situação:\n" + ex.getMessage());
+        } finally {
+        }
+    }
+
+    private void carregaSituacoes() {
+        List<Situacao> todasSituacoes;
+        try {
+            abreSessao();
+            situacoes = new ArrayList();
+
+            SituacaoDao situacaoDao = new SituacaoDaoImpl();
+            todasSituacoes = situacaoDao.listaTodos(session);
+            todasSituacoes.stream().forEach((perf) -> {
+                situacoes.add(new SelectItem(perf.getId(), perf.getNomeSituacao()));
+            });
+        } catch (HibernateException hi) {
+            System.out.println("Erro ao carregar as situações " + hi.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public void cancelarModal() {
+        RequestContext.getCurrentInstance().closeDialog(null);
+    }
+
+    public void limpar() {
+        produto = new Produto();
+        marca = new Marca();
+        situacao = new Situacao();
+    }
+
+    public void excluir() {
+        produto = modelProdutos.getRowData();
+        abreSessao();
+        try {
+            produtoDao.remover(produto, session);
+            produtos.remove(produto);
+            modelProdutos = new ListDataModel(produtos);
+            Mensagem.excluir("Produto");
+            limpar();
         } catch (Exception e) {
-            System.out.println("erro ao pesquisar produto por tipo: " + e.getMessage());
+            System.out.println("erro ao excluir: " + e.getMessage());
         } finally {
             session.close();
         }
@@ -98,39 +197,42 @@ public class ProdutoControle implements Serializable {
             Mensagem.salvar("Produto ");
         } catch (HibernateException ex) {
             Mensagem.mensagemError("Erro ao salvar\nTente novamente");
-            System.err.println("Erro ao pesquisar Produto:\n" + ex.getMessage());
         } finally {
             produto = new Produto();
-
             session.close();
         }
     }
 
-    public void limpar() {
-        produto = new Produto();
-        marca = new Marca();
-        situacao = new Situacao();
+    public void limparTela() {
+        limpar();
     }
 
-    public void excluir() {
-        produto = modelProduto.getRowData();
-        try {
-            abreSessao();
-            produtoDao.remover(produto, session);
-            Mensagem.excluir("Equipamento ");
-        } catch (Exception ex) {
-            System.err.println("Erro ao excluir equipamento\n" + ex.getMessage());
-        } finally {
-            session.close();
-        }
+    //Gettes e Setters
+    public boolean isMostrar_toolbar() {
+        return mostrar_toolbar;
+    }
+
+    public void setMostrar_toolbar(boolean mostrar_toolbar) {
+        this.mostrar_toolbar = mostrar_toolbar;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
     }
 
     public Produto getProduto() {
         if (produto == null) {
             produto = new Produto();
-
         }
         return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
     }
 
     public Marca getMarca() {
@@ -147,29 +249,12 @@ public class ProdutoControle implements Serializable {
     public Situacao getSituacao() {
         if (situacao == null) {
             situacao = new Situacao();
-
         }
         return situacao;
     }
 
     public void setSituacao(Situacao situacao) {
         this.situacao = situacao;
-    }
-
-    public boolean isMostrar_Toolbar() {
-        return mostrar_Toolbar;
-    }
-
-    public void setMostrar_Toolbar(boolean mostrar_Toolbar) {
-        this.mostrar_Toolbar = mostrar_Toolbar;
-    }
-
-    public Session getSession() {
-        return session;
-    }
-
-    public void setSession(Session session) {
-        this.session = session;
     }
 
     public ProdutoDao getProdutoDao() {
@@ -180,8 +265,12 @@ public class ProdutoControle implements Serializable {
         this.produtoDao = produtoDao;
     }
 
-    public void setProduto(Produto produto) {
-        this.produto = produto;
+    public DataModel<Produto> getModelProdutos() {
+        return modelProdutos;
+    }
+
+    public void setModelProdutos(DataModel<Produto> modelProdutos) {
+        this.modelProdutos = modelProdutos;
     }
 
     public List<Produto> getProdutos() {
@@ -192,12 +281,20 @@ public class ProdutoControle implements Serializable {
         this.produtos = produtos;
     }
 
-    public DataModel<Produto> getModelProduto() {
-        return modelProduto;
+    public List<SelectItem> getMarcas() {
+        return marcas;
     }
 
-    public void setModelProduto(DataModel<Produto> modelProduto) {
-        this.modelProduto = modelProduto;
+    public void setMarcas(List<SelectItem> marcas) {
+        this.marcas = marcas;
+    }
+
+    public List<SelectItem> getSituacoes() {
+        return situacoes;
+    }
+
+    public void setSituacoes(List<SelectItem> situacoes) {
+        this.situacoes = situacoes;
     }
 
 }
--- src/main/java/br/com/glp/util/Teste.java
+++ src/main/java/br/com/glp/util/Teste.java
@@ -3,9 +3,12 @@ package br.com.glp.util;
 import br.com.glp.dao.FuncionarioDao;
 import br.com.glp.dao.FuncionarioDaoImpl;
 import br.com.glp.dao.HibernateUtil;
+import br.com.glp.dao.MarcaDao;
+import br.com.glp.dao.MarcaDaoImpl;
 import br.com.glp.dao.ProdutoDao;
 import br.com.glp.dao.ProdutoDaoImpl;
 import br.com.glp.model.Funcionario;
+import br.com.glp.model.Marca;
 import br.com.glp.model.Produto;
 import java.util.List;
 import org.hibernate.Session;
@@ -23,25 +26,36 @@ public class Teste {
 
     private Produto produto;
     private ProdutoDao produtoDao;
+    
+    private Marca marca;
+    private MarcaDao marcaDao;
 
     public Teste() {
 
         funcionarioDao = new FuncionarioDaoImpl();
         produtoDao = new ProdutoDaoImpl();
+        marcaDao = new MarcaDaoImpl();
     }
 
     public void listarTodos() {
 
         session = HibernateUtil.abreSessao();
 
-        List<Produto> produtos = produtoDao.listaTodos(session);
-
-        for (Produto produto1 : produtos) {
-            System.out.println("\n");
-            System.out.println("---------------------------------------");
-            System.out.println("Produto: " + produto1.getNomeProduto());
-            System.out.println("Situação: " + produto1.getSituacao().getNomeSituacao());
-            System.out.println("Marca: " + produto1.getMarca().getNomeMarca());
+//        List<Produto> produtos = produtoDao.listaTodos(session);
+//
+//        for (Produto produto1 : produtos) {
+//            System.out.println("\n");
+//            System.out.println("---------------------------------------");
+//            System.out.println("Produto: " + produto1.getNomeProduto());
+//            System.out.println("Situação: " + produto1.getSituacao().getNomeSituacao());
+//            System.out.println("Marca: " + produto1.getMarca().getNomeMarca());
+//        }
+        
+       List<Marca> marcas =  marcaDao.listaTodos(session);
+       
+        for (Marca marca1 : marcas) {
+            System.out.println("ID: "+marca1.getId());
+            System.out.println("Marca: "+marca1.getNomeMarca());
         }
 
     }
